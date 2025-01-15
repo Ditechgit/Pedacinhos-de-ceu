94cfc621004a81af709b565b49aeb7862e9c962f
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-01-15 16:32:27 -0300

    Criação inicial da seção formulario

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,63 @@ import Logo from "./components/Logo";
 function App() {
   return (
     <div className="m-0 p-0 box-border list-none">
-      <header className="flex justify-between items-center flex-wrap p-5">
+      <header className="flex justify-between items-center flex-wrap p-5 ">
         <div>
           <Logo />
         </div>
         <nav>
           <ul className="flex">
             <li className="ml-2.5">
-              <a className="block p-2.5" href="#">Home</a>
+              <a className="block p-2.5" href="#">
+                Home
+              </a>
             </li>
             <li className="ml-2.5">
-              <a className="block p-2.5" href="#">Serviços</a>
+              <a className="block p-2.5" href="#">
+                Serviços
+              </a>
             </li>
             <li className="ml-2.5">
-              <a className="block p-2.5" href="#">Sobre</a>
+              <a className="block p-2.5" href="#">
+                Sobre
+              </a>
             </li>
             <li className="ml-2.5">
-              <a className="block p-2.5" href="#">Contato</a>
+              <a className="block p-2.5" href="#">
+                Contato
+              </a>
             </li>
           </ul>
         </nav>
       </header>
       <main className="grid grid-cols-2 gap-5 max-w-7xl my-0 mx-auto items-center">
-        <section className="">
-          <div className="">
-            <p className="">eBook Gratis</p>
-            <h2 className="">
+        <section className="grid justify-center bg-gradient-to-r from-slate-500 rounded-md ">
+          <div className="my-8 px-7">
+            <p className="mt-4 text-xl">eBook Grátis</p>
+            <h1 className="text-4xl">
               Receitas de Cookie: 34 ideias para qualquer negócio
-            </h2>
-            <p className="">
+            </h1>
+            <p className="mt-4 text-xl">
               Confira dezenas de receitas sobre cookies, com taxa de sucesso de
               até 95% + dicas de especialistas sobre vendas e organização de
               finanças
             </p>
           </div>
-          <div>
-            <button className="">Baixe Grátis o eBook</button>
-            <p>
-              Acesse o eBook para Black Friday, promções e negócios variados
+          <div className="grid justify-center mt-8 mb-16 p-7 ">
+            <button className="border-2 border-slate-950 rounded-md">
+              <a className="block p-2.5 my-2.5" href="#">
+                Baixe Grátis o eBook
+              </a>
+            </button>
+            <p className="mt-4">
+              Acesse o eBook para Black Friday, promoções e negócios variados.
             </p>
           </div>
         </section>
         <section className="grid justify-center">
           <Cookie />
         </section>
-        <section>
-          {" "}
+        <section className="grid justify-center px-7">
           {/*Aqui fica a primeira parte depois do header*/}
           <div>
             <h2>Aqui terá um subtitulo para enfase do produto</h2>
@@ -59,8 +70,75 @@ function App() {
               <li>Terceira Ideia Introdutoria</li>
             </ul>
           </div>
+        </section>
+        <section className="flex flex-col justify-center border p-7 mt-7 mb-12 max-w-md self-center">
           <div>
-            <h2>Aqui vai ficar o formulario da pagina</h2>
+            <h3 className="text-2xl font-bold">Seu acesso é grátis e rápido!</h3>
+          </div>
+          <div>
+            <p>Preencha em segundos</p>
+          </div>
+          <div>
+            <form action="">
+              <div>
+                <div className="grid">
+                  <label className="mb-1" htmlFor="">Nome*</label>
+                  <input
+                    className="border-2 border-black rounded-md px-2 mb-4"
+                    type="text"
+                    name="name"
+                    placeholder="Nome *"
+                    required="required"
+                  />
+                </div>
+                <div className="grid">
+                  <label className="mb-1" htmlFor="">Email*</label>
+                  <input
+                    className="border-2 border-black rounded-md px-2 mb-4"
+                    type="email"
+                    name="email"
+                    placeholder="Email *"
+                    required="required"
+                  />
+                </div>
+                <div className="grid">
+                  <label className="mb-1" htmlFor="">Empresa*</label>
+                  <input
+                    className="border-2 border-black rounded-md px-2 mb-4"
+                    type="text"
+                    name="company"
+                    placeholder="Empresa *"
+                    required="required"
+                  />
+                </div>
+                <div className="grid">
+                  <label className="mb-1" htmlFor="">1 + 7 = ?</label>
+                  <input
+                    className="border-2 border-black rounded-md px-2 mb-4"
+                    type="text"
+                    name="captcha"
+                    placeholder="1 + 7 = ? *"
+                    required="required"
+                  />
+                </div>
+              </div>
+              <div className="grid font-bold ">
+                <button className="mt-4 py-px px-2.5 border-2 border-black rounded-md">BAIXE GRÁTIS AGORA</button>
+              </div>
+            </form>
+          </div>
+          <div className="my-2.5 ">
+            <p className="text-sm mt-2">
+              Ao preencher o formulário, concordo * em receber comunicações de
+              acordo com meus interesses.
+            </p>
+            <p className="text-sm mt-2">
+              Ao informar meus dados, eu concordo com a Política de privacidade.
+            </p>
+            <p className="text-sm mt-2  ">
+              *Você pode alterar suas permissões de comunicação a qualquer
+              tempo.
+            </p>
           </div>
         </section>
 
--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -2,7 +2,7 @@ import cookie from "../assets/cookie.jpg";
 
 function Cookie() {
   return (
-      <img src={cookie} alt="" className="size-80 rounded-full"/>
+      <img src={cookie} alt="" className="size-96 rounded-full"/>
   );
 }
 
